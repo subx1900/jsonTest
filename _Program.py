6231c8af51a1e313d5033e586dfbafc5200bfb05
subx1900 <subx1900@gmail.com>
2015-11-27 11:18:51 +0200

    Update _Program.py
    Asks for user input (case sensitive)

--- _Program.py
+++ _Program.py
@@ -3,7 +3,11 @@ from jsonDal import JsonDal
 myJsonRun = JsonDal()
 
 res = myJsonRun.ServletNameAndIparamValExist("cofaxTools", "removePageCache")
-
 print res
+
+srvn = raw_input('Enter servlet-name : ')
+iprm = raw_input('Now enter init-param : ')
+
+print myJsonRun.ServletNameAndIparamValExist(srvn, iprm)
 
 #myJsonRun.printJson()
